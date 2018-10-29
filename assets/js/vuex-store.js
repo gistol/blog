8c5f5b914686b19f4f0b2b436fa1e327b48c6034
= <akyuzgurcan@hotmail.com>
2018-10-29 09:07:12 -0400

    The configuration settings are retrieved from the blog.yaml file.

--- assets/js/vuex-store.js
+++ assets/js/vuex-store.js
@@ -1,4 +1,39 @@
+const state = {
+    ga_id: '123123',
+    app_title: '',
+};
+
+const getters = {
+    getGAID(state) {
+        return state.ga_id;
+    },
+    getAppTitle(state) {
+        return state.app_title;
+    }
+};
+
+const mutations = {
+    setGAID(state, ga_id) {
+        state.ga_id = ga_id;
+    },
+    setAppTitle(state, title) {
+        state.app_title = title;
+    }
+};
+
+const actions = {
+    updateGAID({ commit }, ga_id) {
+        commit('setGAID', ga_id);
+    },
+    updateAppTitle({ commit }, title) {
+        commit('setAppTitle', title);
+    }
+};
+
 export default {
-    modules: {},
+    modules: state,
+    getters,
+    mutations,
+    actions,
     strict: true,
 };
